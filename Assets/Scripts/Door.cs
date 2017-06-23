0098c29e85b4cd9b7efff5d181ee3304a817999d
KrisPearson <pearsonk1@hotmail.co.uk>
2017-06-23 18:54:57 +0100

    Doorway Stuff
    - Added particle colour changing
    - Added particle toggle on lock and unlock

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,15 +4,60 @@ public class Door : MonoBehaviour {
 
 	public Colors color;
 	public bool isOpen;
-	public Transform doorObject; //Actualy thing to be enabled and disabled
-	
-	public void trigger(){
+	//public Transform doorObject; //Actualy thing to be enabled and disabled
+
+    private ParticleSystem doorParticleWhite;
+    private ParticleSystem doorParticleColoured;
+
+    void Start() {
+        doorParticleWhite = transform.GetChild(1).GetComponent<ParticleSystem>();
+        doorParticleColoured = transform.GetChild(2).GetComponent<ParticleSystem>();
+        SetParticleColour();
+    }
+
+    public void trigger(){
 		if(isOpen){
-			doorObject.gameObject.SetActive(true);
+			//doorObject.gameObject.SetActive(true);
 			isOpen=false;
 		}else{
-			doorObject.gameObject.SetActive(false);
+			//doorObject.gameObject.SetActive(false);
 			isOpen=true;	
 		}
-	}
+        ToggleParticle();
+    }
+
+    private void ToggleParticle() {
+        var mainDp = doorParticleWhite.main;
+        if (!isOpen) {
+            mainDp.loop = true;
+            doorParticleWhite.Play();
+        }
+        else {
+            mainDp.loop = false;
+        }
+    }
+
+    void SetParticleColour() {
+        var main = doorParticleColoured.main;
+        Color newParticleColour = Color.white;
+
+        switch (color) {
+            case Colors.Blue:
+                newParticleColour = Color.blue;
+                break;
+            case Colors.Green:
+                newParticleColour = Color.green;
+                break;
+            case Colors.Orange:
+                newParticleColour = Color.magenta; //<===== "make me orange!"
+                break;
+            case Colors.Red:
+                newParticleColour = Color.red;
+                break;
+            case Colors.Yellow:
+                newParticleColour = Color.yellow;
+                break;
+        }
+        main.startColor = newParticleColour;
+    }
 }
